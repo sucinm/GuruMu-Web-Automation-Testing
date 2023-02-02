076eb09fae663c217d6804c31e70f233535c733f
sucinm <suci.nascaisar@gmail.com>
2023-02-02 17:44:32 +0700

    template project

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -14,7 +14,7 @@ public class Hooks {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, System.getProperty("user.dir")+  "/driver/chromedriver");
 
         webDriver = new ChromeDriver();
-        String appUrl = "https://www.saucedemo.com/";
+        String appUrl = "https://devmyproject.site/";
         webDriver.get(appUrl);
         webDriver.manage().window().maximize();
     }
