4511e13cbb000337458b3352e1e45426d82d156b
dlouistandio <davidlouistandio@gmail.com>
2023-02-14 23:38:37 +0700

    Fixing steps and scenario for register

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -5,7 +5,6 @@ import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
-
 public class Hooks {
     public static WebDriver webDriver;
 
